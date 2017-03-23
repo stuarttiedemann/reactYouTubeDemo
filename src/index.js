d8143ce8655a587e9bf2ffd7af12cd5245aef5cd
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-03-23 16:21:47 -0500

    updated index files and set up youtube api call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
-const App = () => {
-  return <div>Hi!</div>;
+const API_KEY = 'AIzaSyBoJnsdq8A_ydAtuzoWVKN8aIy8kWRW34g';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({ videos: videos });
+      this.setState({ videos });
+      console.log(videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+         <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
