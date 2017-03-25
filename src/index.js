2f1458d129b58e500c6bfc2af2a2a83ec42be4ec
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-03-24 19:10:54 -0500

    Add video components and api call

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBoJnsdq8A_ydAtuzoWVKN8aIy8kWRW34g';
 
@@ -9,11 +11,21 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      // this.setState({ videos: videos });
-      this.setState({ videos });
+    this.videoSearch('surfboard');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos, 
+        selectedVideo: videos[0]
+      });
+
       console.log(videos);
     });
   }
@@ -21,7 +33,11 @@ class App extends Component {
   render() {
     return (
       <div>
-         <SearchBar />
+         <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+         <VideoDetail video={this.state.selectedVideo}/>
+         <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+            videos={this.state.videos} />
       </div>
     );
   }
